9fad0e259609311c9170504ff390fdb2e40a6142
cedarLongballa <longbacedar@gmail.com>
2025-04-05 14:54:10 -0500

    Added a contact interface with the needed variables and implemented how they will show on the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'assignment-4';
+
+  contacts: Contact[] = [];
 }
---
+++ src/app/contact.ts
@@ -0,0 +1,6 @@
+export interface Contact {
+    firstName: string;
+    lastName: string;
+    number: number;
+    email: string;
+}
